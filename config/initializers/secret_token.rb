8b1dc050865bb3575e50cfb99d81fffaf9bbb1f0
Jason Laster <Jason.laster.11@gmail.com>
2011-09-10 16:12:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threedegrees::Application.config.secret_token = 'b261e0ceab3bd5914c5499fabdb190c00a0f1d8f4cf64ceb437ba63862c2def2e5de33f9e2e64d764d820c80c1dbcf5cd617ae6fd1baad186dd5d5b2c4a4c661'
